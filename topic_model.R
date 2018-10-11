56b0a0adcb5883b62ab6e834d63dcfcc201b9d8a
Justin <jniestroy@gmail.com>
2018-10-11 09:57:31 -0400

    update topic model

--- topic_model.R
+++ topic_model.R
@@ -8,13 +8,17 @@ library(plyr)
 
 setwd('C:\\Users\\x1\\Documents\\SYS 6018\\blog')
 train <- readr::read_csv('train.csv')
+train <- transform(train, total= str_count(text))
+#count word length of post
+train <- transform(train,work = str_count(text,'work'))
+#count # of times post says work
 
 #makes df with all texts from each user into one variable
 df <- aggregate(text ~ user.id, data = train, toString)
 names(df) <- c('doc_id','text')
 
 #creates another data frame 1 obs per user includes most common topic and total number of posts
-train1 <- ddply(train,.(user.id),summarize,age = mean(age),number=length(user.id),topic={
+train1 <- ddply(train,.(user.id),summarize,age = mean(age),sum_work=sum(work),number=length(user.id),topic={
   tt <- table(topic)
   names(tt)[which.max(tt)]
 })
@@ -46,10 +50,12 @@ row.sums = apply(blog.clean.tf, 1, sum)
 blogs = blogs[row.sums > 0]
 blog.clean.tf = blog.clean.tf[row.sums > 0,]
 
-# train topic model with 10 topics
+# train topic model with 3 topics
 topic.model = LDA(blog.clean.tf, 3)
 
-# look at the top 10 words within the first 5 topics
+#Tried varying number of topics many times 3 outperformed 
+
+# look at the top 10 words within the first 3 topics
 terms(topic.model, 10)[,1:3]
 
 pratice_model_training <- total[c(1:50,6000:6050,12830:12880),]
@@ -79,10 +85,32 @@ total <- cbind(total,probs)
 saveRDS(total,'total_with_probs.rds')
 total_train <-  readRDS('total_with_probs.rds')
 names(total_train) <- c(names(total_train)[-c(6,7,8)],'p1','p2','p3')
-lm.mod_total<- lm(age~as.factor(topic)+number+p1+p2,data = total_train)
+
+sum(is.na(total_train$sum_work))
+
+sum_work <- train1$sum_work
+total_train <- cbind(total_train,sum_work)
+total_train$sum_work[which(is.na(total_train$sum_work))] <- 0 
+lm.mod_total<- lm(age~as.factor(topic)+number+p1+p2+sum_work,data = total_train)
 summary(lm.mod_total)
 #Adjusted R-squared:  0.5036 for 3 topics 10.4 error
 
+#Cross Validation
+
+library(boot)
+set.seed(1995)
+glm.fit=glm(age~as.factor(topic)+number+p1+p2,data = total_train)
+cv.err=cv.glm(na.omit(total_train),glm.fit)
+cv.err
+cv.error.10=rep(0,10)
+for (i in 1:10){
+  glm.fit=glm(age~as.factor(topic)+number+p1+p2+sum_work,data = total_train)
+  cv.error.10[i]=cv.glm(na.omit(total_train),glm.fit,K=10)$delta
+  print(i)
+}
+cv.error.10
+#44.84147 44.86881 44.90278 44.85437 44.84654 44.85225 44.82343 44.82200 44.88971 44.90306 w/o sum_work
+#44.85597 44.92927 44.86434 44.88540 44.92996 44.90370 44.90964 44.89295 44.86599 44.88892 w/ sum work
 
 ##############################
 #Testing time
